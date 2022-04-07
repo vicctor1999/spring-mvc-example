0859380c527188356476910ff40bf607cdad2879
vicctor1999 <victorperezalfayate@gmail.com>
2022-04-07 13:59:09 +0200

    fin clase 1 spring

---
+++ src/main/java/com/formacion/nttdata/hello/model/User.java
@@ -0,0 +1,22 @@
+package com.formacion.nttdata.hello.model;
+
+public class User {
+	String userName;
+	String userSurname;
+	
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getUserSurname() {
+		return userSurname;
+	}
+	public void setUserSurname(String userSurname) {
+		this.userSurname = userSurname;
+	}
+	
+	
+	
+}
